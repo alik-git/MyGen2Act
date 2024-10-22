6a6559e8d124103721c37581783640578e46b2a8
Ali K <alihusein.kuwajerwala@umontreal.ca>
2024-10-22 14:51:20 -0700

    modify open-flamingo code to match our use case

--- src/perceiver_resampler.py
+++ src/perceiver_resampler.py
@@ -23,16 +23,19 @@ def FeedForward(dim, mult=4):
         nn.Linear(inner_dim, dim, bias=False),
     )
 
-# gated cross attention
-class MaskedCrossAttention(nn.Module):
+# Cross Attention without media variables
+class CrossAttention(nn.Module):
+    """ 
+    In the open-flamingo implementation, they are wanting the text tokens to attend to the media tokens.
+    In out context, we want the latent tokens to attend to the media tokens. This is the key difference.
+    """
     def __init__(
         self,
         *,
         dim,
-        dim_visual,
+        dim_context, # their dim_visual is our dim_context
         dim_head=64,
         heads=8,
-        only_attend_immediate_media=True,
     ):
         super().__init__()
         self.scale = dim_head**-0.5
@@ -42,106 +45,52 @@ class MaskedCrossAttention(nn.Module):
         self.norm = nn.LayerNorm(dim)
 
         self.to_q = nn.Linear(dim, inner_dim, bias=False)
-        self.to_kv = nn.Linear(dim_visual, inner_dim * 2, bias=False)
+        self.to_kv = nn.Linear(dim_context, inner_dim * 2, bias=False) # their dim_visual is our dim_context
         self.to_out = nn.Linear(inner_dim, dim, bias=False)
 
-        # whether for text to only attend to immediate preceding image, or all previous images
-        self.only_attend_immediate_media = only_attend_immediate_media
-
-    def forward(self, x, media, media_locations=None, use_cached_media=False):
+    def forward(self, x, context): # their media is our context
         """
         Args:
-            x (torch.Tensor): text features
-                shape (B, T_txt, D_txt)
-            media (torch.Tensor): image features
-                shape (B, T_img, n, D_img) where n is the dim of the latents
-            media_locations: boolean mask identifying the media tokens in x
-                shape (B, T_txt)
-            use_cached_media: bool
-                If true, treat all of x as if they occur after the last media
-                registered in media_locations. T_txt does not need to exactly
-                equal media_locations.shape[1] in this case
+            x (torch.Tensor): input features
+                shape (b, n, d)
+            context (torch.Tensor): context features
+                shape (b, m, d)
         """
-
-        if not use_cached_media:
-            assert (
-                media_locations.shape[1] == x.shape[1]
-            ), f"media_location.shape is {media_locations.shape} but x.shape is {x.shape}"
-
-        T_txt = x.shape[1]
-        _, T_img, n = media.shape[:3]
         h = self.heads
 
         x = self.norm(x)
 
         q = self.to_q(x)
-        media = rearrange(media, "b t n d -> b (t n) d")
 
-        k, v = self.to_kv(media).chunk(2, dim=-1)
+        k, v = self.to_kv(context).chunk(2, dim=-1) # their media is our context
         q, k, v = rearrange_many((q, k, v), "b n (h d) -> b h n d", h=h)
 
         q = q * self.scale
 
-        sim = einsum("... i d, ... j d -> ... i j", q, k)
-
-        if exists(media_locations):
-            media_time = torch.arange(T_img, device=x.device) + 1
-
-            if use_cached_media:
-                # text time is set to the last cached media location
-                text_time = repeat(
-                    torch.count_nonzero(media_locations, dim=1),
-                    "b -> b i",
-                    i=T_txt,
-                )
-            else:
-                # at each boolean of True, increment the time counter (relative to media time)
-                text_time = media_locations.cumsum(dim=-1)
-
-            # text time must equal media time if only attending to most immediate image
-            # otherwise, as long as text time is greater than media time (if attending to all previous images / media)
-            mask_op = torch.eq if self.only_attend_immediate_media else torch.ge
-
-            text_to_media_mask = mask_op(
-                rearrange(text_time, "b i -> b 1 i 1"),
-                repeat(media_time, "j -> 1 1 1 (j n)", n=n),
-            )
-            sim = sim.masked_fill(~text_to_media_mask, -torch.finfo(sim.dtype).max)
-
-        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        sim = einsum("b h i d, b h j d -> b h i j", q, k)
         attn = sim.softmax(dim=-1)
 
-        if exists(media_locations) and self.only_attend_immediate_media:
-            # any text without a preceding media needs to have attention zeroed out
-            text_without_media_mask = text_time == 0
-            text_without_media_mask = rearrange(
-                text_without_media_mask, "b i -> b 1 i 1"
-            )
-            attn = attn.masked_fill(text_without_media_mask, 0.0)
-
-        out = einsum("... i j, ... j d -> ... i d", attn, v)
+        out = einsum("b h i j, b h j d -> b h i d", attn, v)
         out = rearrange(out, "b h n d -> b n (h d)")
         return self.to_out(out)
 
-
+# Gated Cross Attention Block without media variables
 class GatedCrossAttentionBlock(nn.Module):
     def __init__(
         self,
         *,
         dim,
-        dim_visual,
+        dim_context, # their dim_visual is our dim_context
         dim_head=64,
         heads=8,
         ff_mult=4,
-        only_attend_immediate_media=True,
     ):
         super().__init__()
-        self.attn = MaskedCrossAttention(
+        self.attn = CrossAttention(
             dim=dim,
-            dim_visual=dim_visual,
+            dim_context=dim_context, # their dim_visual is our dim_context
             dim_head=dim_head,
             heads=heads,
-            only_attend_immediate_media=only_attend_immediate_media,
         )
         self.attn_gate = nn.Parameter(torch.tensor([0.0]))
 
@@ -151,16 +100,12 @@ class GatedCrossAttentionBlock(nn.Module):
     def forward(
         self,
         x,
-        media,
-        media_locations=None,
-        use_cached_media=False,
+        context, # their media is our context
     ):
         x = (
             self.attn(
                 x,
-                media,
-                media_locations=media_locations,
-                use_cached_media=use_cached_media,
+                context, # their media is our context
             )
             * self.attn_gate.tanh()
             + x
@@ -170,39 +115,27 @@ class GatedCrossAttentionBlock(nn.Module):
         return x
 
 
-
+# Perceiver Resampler adjusted to your use case
 class PerceiverResampler(nn.Module):
     def __init__(
         self,
         *,
         dim,
-        depth=6,
+        depth=2, # from the Gen2Act paper
         dim_head=64,
         heads=8,
         num_latents=64,
-        max_num_media=None,
-        max_num_frames=None,
         ff_mult=4,
     ):
         super().__init__()
         self.latents = nn.Parameter(torch.randn(num_latents, dim))
-        self.frame_embs = (
-            nn.Parameter(torch.randn(max_num_frames, dim))
-            if exists(max_num_frames)
-            else None
-        )
-        self.media_time_embs = (
-            nn.Parameter(torch.randn(max_num_media, 1, dim))
-            if exists(max_num_media)
-            else None
-        )
 
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(
                 nn.ModuleList(
                     [
-                        PerceiverAttention(dim=dim, dim_head=dim_head, heads=heads),
+                        GatedCrossAttentionBlock(dim=dim, dim_head=dim_head, heads=heads, dim_context=dim, ff_mult=ff_mult),
                         FeedForward(dim=dim, mult=ff_mult),
                     ]
                 )
@@ -213,26 +146,15 @@ class PerceiverResampler(nn.Module):
     def forward(self, x):
         """
         Args:
-            x (torch.Tensor): image features
-                shape (b, T, F, v, D)
+            x (torch.Tensor): input features
+                shape (b, n, d)
         Returns:
-            shape (b, T, n, D) where n is self.num_latents
+            shape (b, num_latents, d)
         """
-        b, T, F, v = x.shape[:4]
-
-        # frame and media time embeddings
-        if exists(self.frame_embs):
-            frame_embs = repeat(self.frame_embs[:F], "F d -> b T F v d", b=b, T=T, v=v)
-            x = x + frame_embs
-        x = rearrange(
-            x, "b T F v d -> b T (F v) d"
-        )  # flatten the frame and spatial dimensions
-        if exists(self.media_time_embs):
-            x = x + self.media_time_embs[:T]
-
+        b, n, d = x.shape
         # blocks
-        latents = repeat(self.latents, "n d -> b T n d", b=b, T=T)
-        for attn, ff in self.layers:
-            latents = attn(x, latents) + latents
+        latents = repeat(self.latents, "n d -> b n d", b=b)
+        for cross_attn, ff in self.layers:
+            latents = cross_attn(latents, x) + latents # because they are using perceiver attention here and we are using gated cross attention
             latents = ff(latents) + latents
         return self.norm(latents)
